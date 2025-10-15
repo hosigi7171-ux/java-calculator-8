1123e37918d72aac796d32a35a08b777ed74e325
kimye0808 <kyh011@hanyang.ac.kr>
2025-10-15 14:00:46 +0900

    feat(input): inputReader 기능 구현
    inputReader 객체 생성 기능 구현, main에 해당 객체 사용 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,6 +2,9 @@ package calculator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String inputStr;
+
+        InputReader inputReader = new InputReader();
+        inputStr = inputReader.getInput();
     }
 }
