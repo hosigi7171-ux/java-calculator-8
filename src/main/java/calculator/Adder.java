4a0ce8809cb7ae3bcc9e3741e75c653f4b72918e
hosigi7171-ux <hosigi7171@gmail.com>
2025-10-15 16:30:13 +0900

    feat: implement Adder
    입력받은 정수 리스트를 더해주는 Adder 구현

---
+++ src/main/java/calculator/Adder.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import java.util.List;
+
+public class Adder {
+    /**
+     * 정수 리스트를 입력받아 합을 구하는 메서드
+     * @param numbers 정수 리스트
+     * @return 정수들의 합
+     */
+    public int sumIntList(List<Integer> numbers){
+        int sum = 0;
+        for(int num : numbers){
+            sum += num;
+        }
+        return sum;
+    }
+}
